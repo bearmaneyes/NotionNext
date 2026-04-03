04edf4385f989b1aa4a317ec34fd98c45011817e
bearmaneyes <75205214+bearmaneyes@users.noreply.github.com>
2026-04-03 15:39:55 +0800

    Create config.js

---
+++ themes/elegance/config.js
@@ -0,0 +1,6 @@
+// themes/elegance/config.js
+const CONFIG = {
+  // 这里可以放一些主题专属的配置，比如 Banner 图片地址
+  ELEGANCE_BANNER_IMAGE: 'https://images.unsplash.com/photo-1469474968028-56623f02e42e?q=80&w=2074&auto=format&fit=crop',
+}
+export default CONFIG

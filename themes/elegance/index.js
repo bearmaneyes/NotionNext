6298e3ea3b7fb5c4ea7c325163974af0ab77adff
bearmaneyes <75205214+bearmaneyes@users.noreply.github.com>
2026-04-04 20:34:59 +0800

    Update index.js

--- themes/elegance/index.js
+++ themes/elegance/index.js
@@ -2,25 +2,29 @@ import CONFIG from './config'
 import LayoutIndex from './LayoutIndex'
 import LayoutSlug from './LayoutSlug'
 
-// 【关键修复】从自带的 hexo 主题借用你还没写的必须组件，防止网页崩溃！
-import LayoutBase from '../hexo/LayoutBase'
-import LayoutSearch from '../hexo/LayoutSearch'
-import LayoutArchive from '../hexo/LayoutArchive'
-import Layout404 from '../hexo/Layout404'
-import LayoutCategoryIndex from '../hexo/LayoutCategoryIndex'
-import LayoutCategory from '../hexo/LayoutCategory'
-import LayoutTagIndex from '../hexo/LayoutTagIndex'
-import LayoutTag from '../hexo/LayoutTag'
-import LayoutPage from '../hexo/LayoutPage'
+// 【关键修复】使用动态导入，如果当前主题没写，就回退到默认主题的组件
+import { useLayoutByTheme } from '@/themes/theme'
+import * as ThemeComponents from '@theme-components'
+
+// 动态获取兜底组件
+const LayoutBase = ThemeComponents.LayoutBase
+const LayoutSearch = ThemeComponents.LayoutSearch
+const LayoutArchive = ThemeComponents.LayoutArchive
+const Layout404 = ThemeComponents.Layout404
+const LayoutCategoryIndex = ThemeComponents.LayoutCategoryIndex
+const LayoutCategory = ThemeComponents.LayoutCategory
+const LayoutTagIndex = ThemeComponents.LayoutTagIndex
+const LayoutTag = ThemeComponents.LayoutTag
+const LayoutPage = ThemeComponents.LayoutPage
 
 export {
-  CONFIG as THEME_CONFIG, // theme.js 第 21 行强依赖 THEME_CONFIG 这个名字
+  CONFIG as THEME_CONFIG,
   CONFIG,
-  LayoutBase, // theme.js 第 50 行强依赖这个
+  LayoutBase,
   LayoutIndex,
   LayoutSearch,
   LayoutArchive,
-  LayoutSlug, // theme.js 第 73 行强依赖这个作为兜底
+  LayoutSlug,
   Layout404,
   LayoutCategoryIndex,
   LayoutCategory,
